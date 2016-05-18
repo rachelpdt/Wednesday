e5c8140b798917d5669ad86c84058d4545a22264
rachelpdt <rachelsdf39@gmail.com>
2016-05-17 21:16:32 -0400

    Completed Course

---
+++ challenge_01/js/students.js
@@ -0,0 +1,27 @@
+var students = [
+  {
+    name: 'Rachel',
+    track: 'Front End Develpment',
+    acheievements: 159,
+    points: 15906
+  },
+  {
+    name: 'Edward',
+    track: 'Ruby On Rails Develpment',
+    acheievements: 145,
+    points: 13934
+  },
+  {
+    name: 'Jan',
+    track: 'iOS Develpment with Swift',
+    acheievements: 137,
+    points: 12546
+  },
+  {
+    name: 'Sarah',
+    track: 'iOS Develpment with Swift',
+    acheievements: 150,
+    points: 14567
+  }
+];
+console.log(students);
