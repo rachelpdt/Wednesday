30b454074e4fbfc92baf1e9cd640deaf692f581b
rachelpdt <rachelsdf39@gmail.com>
2016-06-29 21:45:36 -0400

    Project work and Treehouse practice

---
+++ Modified Its a Trap/js/app.js
@@ -0,0 +1,10 @@
+//Hide Warning
+//jQuery(".warning").hide(); + jQuery(".warning").show("slow");
+//Show Warning Slowly
+
+
+function myCode() {
+  $(".greeting").hide().show("slow");
+}
+
+ $(document).ready(myCode);
---
+++ project_start/js/app.js
@@ -0,0 +1,2 @@
+//Problem: User interaction doesn't provide desired results.
+//Solution: Add interactivity so that the user can manage daily tasks.
